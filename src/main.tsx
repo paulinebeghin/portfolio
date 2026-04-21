b0792e70115b7ea14e19810fd2987fbbd44d5d2f
pauline beghin <paulinebeghin7@gmail.com>
2026-04-21 13:05:39 +0200

    :sparkles: adding route file and router dependency dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import './index.css' 
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './route'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
